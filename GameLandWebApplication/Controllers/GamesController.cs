0ffb0bd3781f565bc7eb7a25c450b023fc15a8c5
vladzio228 <vladyslavpetriv@gmail.com>
2021-03-03 17:55:59 +0200

    4-th adecvat commit

--- GameLandWebApplication/Controllers/GamesController.cs
+++ GameLandWebApplication/Controllers/GamesController.cs
@@ -22,7 +22,7 @@ namespace GameLandWebApplication.Controllers
         public async Task<IActionResult> Index()
         {
             var gameLandContext = _context.Games.Include(g => g.Developer).Include(g => g.Publisher).Include(g => g.GamesGenres).ThenInclude(g => g.Genre).Include(g => g.GamesPlatforms).ThenInclude(g => g.Platform).
-            Include(g => g.GamesSystemRequirements).ThenInclude(g=>g.SystemRequirement).Include(g=>g.GamesUsers);           
+            Include(g => g.GamesSystemRequirements).ThenInclude(g => g.SystemRequirement).Include(g => g.GamesUsers).ThenInclude(g => g.User);           
             return View(await gameLandContext.ToListAsync());
         }
 
@@ -40,6 +40,7 @@ namespace GameLandWebApplication.Controllers
                 .Include(g => g.GamesGenres).ThenInclude(g => g.Genre)
                 .Include(g => g.GamesPlatforms).ThenInclude(g => g.Platform)
                 .Include(g => g.GamesSystemRequirements).ThenInclude(g => g.SystemRequirement)
+                .Include(g => g.GamesUsers).ThenInclude(g => g.User)
                 .FirstOrDefaultAsync(m => m.GameId == id);
             if (game == null)
             {
