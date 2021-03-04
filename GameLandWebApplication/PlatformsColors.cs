2a531767ffd1607258d4737e8f0f60ad53b45fc2
vladzio228 <vladyslavpetriv@gmail.com>
2021-03-05 01:52:20 +0200

    changed interface of Top, added topscores

--- GameLandWebApplication/PlatformsColors.cs
+++ GameLandWebApplication/PlatformsColors.cs
@@ -10,21 +10,29 @@ namespace GameLandWebApplication
         public static string ToColor(string platform)
         {
             switch(platform){
-                case "PC":             return "#296fda";                  
-                case "PS3":            return "#23527c";                 
-                case "PS4":            return "#23527c";                   
-                case "PS5":            return "#23527c";                    
-                case "Xbox":           return "#0ac70a";                  
-                case "Xbox 360":       return "#0ac70a";                   
-                case "Xbox One":       return "#0ac70a";                   
-                case "Xbox Series X":  return "#0ac70a";                
-                case "Xbox Series S":  return "#0ac70a";                 
+                case "PC":              return "#296fda";                  
+                case "PS3":             return "#23527c";                 
+                case "PS4":             return "#23527c";                   
+                case "PS5":             return "#23527c";                    
+                case "Xbox":            return "#0ac70a";                  
+                case "Xbox 360":        return "#0ac70a";                   
+                case "Xbox One":        return "#0ac70a";                   
+                case "Xbox Series X":   return "#0ac70a";                
+                case "Xbox Series S":   return "#0ac70a";                 
                 case "Nintendo Switch": return "#ed1c24";                  
-                case "iOS":            return "#fff";                
-                case "Android":        return "#b0ef42";                  
-                default:               return "#296fda";
+                case "iOS":             return "#D201EB";                
+                case "Android":         return "#b0ef42";                  
+                default:                return "#296fda";
                     
             }
         }
+        public static string TopColors(int number)
+        {
+            return number == 1 ? "#E0E900" :
+                   number == 2 ? "#A6C3C4" :
+                   number == 3 ? "#E0A12B" :
+                                 "#E5DFDF" ;
+        }
     }
+    
 }
