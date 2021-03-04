8653a58ddc60a8d7e43316daa7a9bdbd5f1088f5
vladzio228 <vladyslavpetriv@gmail.com>
2021-03-04 13:38:47 +0200

    added colors for every game platform, changed Delete.cshtml

---
+++ GameLandWebApplication/PlatformsColors.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameLandWebApplication
+{
+    public static class PlatformsColors
+    {
+        public static string ToColor(string platform)
+        {
+            switch(platform){
+                case "PC":             return "#296fda";                  
+                case "PS3":            return "#23527c";                 
+                case "PS4":            return "#23527c";                   
+                case "PS5":            return "#23527c";                    
+                case "Xbox":           return "#0ac70a";                  
+                case "Xbox 360":       return "#0ac70a";                   
+                case "Xbox One":       return "#0ac70a";                   
+                case "Xbox Series X":  return "#0ac70a";                
+                case "Xbox Series S":  return "#0ac70a";                 
+                case "Nintendo Switch": return "#ed1c24";                  
+                case "iOS":            return "#fff";                
+                case "Android":        return "#b0ef42";                  
+                default:               return "#296fda";
+                    
+            }
+        }
+    }
+}
